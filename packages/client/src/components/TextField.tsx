953dcf14ddde0bd0589cff756c29bdd7f558bea7
Albert Francisco <albertfrancisco124@gmail.com>
2023-08-11 21:26:04 +0800

    fix: remove unnecessary components and files

--- packages/client/src/components/TextField.tsx
+++
@@ -1,8 +0,0 @@
-import { cva } from "class-variance-authority";
-import { TextInput, TextInputProps } from "flowbite-react";
-
-const TextField = (props: { Input: typeof TextInput }) => {
-  return <TextInput {...defaultProps}></TextInput>;
-};
-
-export default TextField;
